cff56c563fc71359efc0b83fe517965d6331e120
Rafael Neuwirth Swierczynski <channelrns@gmail.com>
2022-08-16 14:48:26 -0300

    Initial Commit

---
+++ TechQuiz/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TechQuiz
+//
+//  Created by Rafael Neuwirth Swierczynski on 16/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TechQuiz/TechQuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  TechQuizApp.swift
+//  TechQuiz
+//
+//  Created by Rafael Neuwirth Swierczynski on 16/08/22.
+//
+
+import SwiftUI
+
+@main
+struct TechQuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
